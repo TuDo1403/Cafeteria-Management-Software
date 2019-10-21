f62aaac1d69d1928a3f4f64ea7ad16c518fc857d
thanhchinguyenpk <nctnct1603@gmail.com>
2019-10-21 21:56:03 +0700

    add classes

---
+++ source/UICoffeeManagement/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaoClass
+{
+    class Item:Product
+    {
+        private int kind;
+
+    
+        public Item(string ID, string NAME, string PRICE, int NUMBER,int KIND):base(ID,NAME,PRICE,NUMBER)
+        {
+            kind = KIND;
+        }
+        public override float countMoney()
+        {
+            return number * 100000;
+        }
+    }
+}
