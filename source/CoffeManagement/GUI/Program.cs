021dce0baae9db632dfdec7018518962c4b08889
thanhchinguyenpk <nctnct1603@gmail.com>
2019-11-21 19:06:19 +0700

    logIn form

--- source/CoffeManagement/GUI/Program.cs
+++ source/CoffeManagement/GUI/Program.cs
@@ -16,7 +16,7 @@ namespace CoffeManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            Application.Run(new LogIn());
         }
     }
 }
